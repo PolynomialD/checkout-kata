34b4e91c3ffaf4acad94b61bfacc83cb903d2506
Unknown <steven.grindey@gmail.com>
2018-06-02 11:27:09 +0100

    put elements in variables

--- main.js
+++ main.js
@@ -4,19 +4,24 @@ var C40 = 0
 var T34 = 0
 var receipt = ''
 
+var totalElement =  document.getElementById("total-hidden")
+var receiptElement = document.getElementById("receipt-hidden")
+var errorElement = document.getElementById("error-hidden")
+var userInputElement = document.getElementById("checkoutUserInput")
+
 function addToList(SKUButton) {
-  var SKU = document.getElementById("checkoutUserInput").value
+  var SKU = userInputElement.value
        if((SKU || SKUButton) === 'A99') {A99 += 1, addToReceipt(50)}
   else if((SKU || SKUButton) === 'B15') {B15 += 1, addToReceipt(30)}
   else if((SKU || SKUButton) === 'C40') {C40 += 1, addToReceipt(60)}
   else if((SKU || SKUButton) === 'T34') {T34 += 1, addToReceipt(99)}
-  else {document.getElementById("error-hidden").innerHTML = 'Not a valid SKU'}
+  else {errorElement.innerHTML = 'Not a valid SKU'}
 }
 
 function Total() {
   total = (A99 * 50) + (B15 * 30) + (C40 * 60) + (T34 * 99) -
   ((Math.floor(A99/3)) * 20) - ((Math.floor(B15/2)) * 15)
-  document.getElementById("total-hidden").innerHTML = total
+  totalElement.innerHTML = total
 }
 
 function reset() {
@@ -25,8 +30,8 @@ function reset() {
   C40 = 0
   T34 = 0
   resetUserInput()
-  document.getElementById("total-hidden").innerHTML = ''
-  document.getElementById("receipt-hidden").innerHTML = ''
+  totalElement.innerHTML = ''
+  receiptElement.innerHTML = ''
   receipt = ''
 }
 
@@ -39,14 +44,14 @@ function addToReceipt(amount) {
     receipt += ' -15'
   }
   else {}
-  document.getElementById("receipt-hidden").innerHTML = receipt
+  receiptElement.innerHTML = receipt
 }
 
 function resetUserInput() {
-  document.getElementById("checkoutUserInput").value = ''
-  document.getElementById("error-hidden").innerHTML = ''
+  userInputElement.value = ''
+  errorElement.innerHTML = ''
 }
 
 function resetError() {
-  document.getElementById("error-hidden").innerHTML = ''
+  errorElement.innerHTML = ''
 }
